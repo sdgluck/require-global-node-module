3b161931974df3f4292b4689d43af9ca9cf0ea1d
laggingreflex <laggingreflex@gmail.com>
2017-09-02 17:46:49 +0530

    add resolve

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ var fs = require('fs')
 
 var globalModulesPath = String(exec('npm root -g')).replace(/\r?\n|\r/g, '')
 
+exports.resolve = function globalResolve (module) {
+  return path.resolve(globalModulesPath, module)
+}
+
 module.exports = function requireLocalNodeModule (module) {
   if (typeof module !== 'string' || !module.length) {
     throw new Error('Expecting module to be non-empty string')
   }
 
-  return require(path.resolve(globalModulesPath, module))
+  return require(exports.resolve(module))
 }
