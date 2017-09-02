5cd30aa67f5cfe19dab7336826711dbfd9d45146
laggingreflex <laggingreflex@gmail.com>
2017-09-02 17:44:51 +0530

    cache globalModulesPath
    no need to run it on every require call

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var exec = require('child_process').execSync
 var path = require('path')
 var fs = require('fs')
 
+var globalModulesPath = String(exec('npm root -g')).replace(/\r?\n|\r/g, '')
+
 module.exports = function requireLocalNodeModule (module) {
   if (typeof module !== 'string' || !module.length) {
     throw new Error('Expecting module to be non-empty string')
   }
-
-  var globalModulesPath = String(exec('npm root -g')).replace(/\r?\n|\r/g, '')
 
   return require(path.resolve(globalModulesPath, module))
 }
